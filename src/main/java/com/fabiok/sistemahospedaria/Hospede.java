68b5e126786e90d27caecddddf7acba4a7f7b05f
Fábio Mizo Guti <140606120+Fabio-k@users.noreply.github.com>
2025-10-30 22:42:48 +0000

    feat: adiciona entidade hospede

---
+++ src/main/java/com/fabiok/sistemahospedaria/Hospede.java
@@ -0,0 +1,12 @@
+package com.fabiok.sistemahospedaria;
+
+import java.time.LocalDate;
+
+public class Hospede {
+	private String id;
+	private String nomeCompleto;
+	private String cpf;
+	private LocalDate dataNascimento;
+	private String telefone;
+	private String email;
+}
