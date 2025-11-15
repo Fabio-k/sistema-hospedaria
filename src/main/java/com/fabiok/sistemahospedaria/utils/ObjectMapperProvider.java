7ea53c5b15ad3ac359a46a32ced9190de8c9cb11
fabio-k <fabiokmain@gmail.com>
2025-11-15 20:10:33 -0300

    fix muda a forma como datas são retornadas

--- src/main/java/com/fabiok/sistemahospedaria/utils/ObjectMapperProvider.java
+++ src/main/java/com/fabiok/sistemahospedaria/utils/ObjectMapperProvider.java
@@ -1,10 +1,11 @@
 package com.fabiok.sistemahospedaria.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperProvider {
-    private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
     public static ObjectMapper getMapper(){
         return mapper;
